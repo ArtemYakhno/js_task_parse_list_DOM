ef6d81da64befba82afd4e90ffb03cc1a903798c
Artem Yakhno <artem.yakhno.ua@gmail.com>
2025-10-07 23:03:47 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+function sortList(array) {
+  array.sort((el1, el2) => {
+    const salaryEl1 = Number(el1.dataset.salary.replace(/[^0-9.-]/g, ''));
+    const salaryEl2 = Number(el2.dataset.salary.replace(/[^0-9.-]/g, ''));
+
+    return salaryEl2 - salaryEl1;
+  });
+
+  return array;
+}
+
+function getEmployees(array) {
+  return array.map((el) => {
+    return {
+      name: el.innerHTML,
+      position: el.dataset.position,
+      salary: Number(el.dataset.salary.replace(/[^0-9.-]/g, '')),
+      age: +el.dataset.age,
+    };
+  });
+}
+
+const list = document.querySelector('ul');
+const employees = document.querySelectorAll('ul li');
+const sortArray = sortList([...employees]);
+
+list.innerHTML = '';
+
+sortArray.forEach((el) => {
+  list.append(el);
+});
+
+getEmployees([...employees]);
